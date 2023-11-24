dfcb5d0b56beb9a9c082563688df6f7bf07bfe1e
Lucas França <lucasviniciuspessoaf@gmail.com>
2023-11-24 00:25:39 -0300

    feat: add initial folders

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./pages/Details/index";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
+
+import theme from "./styles/theme";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function App() {
+  return <Container>APP</Container>;
+}
