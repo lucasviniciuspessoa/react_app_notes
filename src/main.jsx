f6bafe22ea2ac331ceea0cd996358554ebb3ed45
Lucas França <lucasviniciuspessoaf@gmail.com>
2023-11-26 17:05:36 -0300

    update: att folder

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { App } from "./pages/Details/index";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 
-import theme from "./styles/theme";
+import theme  from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
